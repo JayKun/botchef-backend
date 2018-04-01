e399bfdb69648e58a3ce02f27dd3c935d2e7d132
Jun <junkaiong11@gmail.com>
2018-04-01 04:59:24 +0000

    get recipes

--- data/populate_recipes.py
+++ data/populate_recipes.py
@@ -20,29 +20,31 @@ with open('recipes.json') as json_data:
     for key in d.keys():
         recipe_data = d[key]
         ingredients = []
-        for ingredient in recipe_data['ingredients']:
-            m = re.search('(?P<name>[a-zA-Z 0-9]+) ADVERTISEMENT', ingredient)
-            if(m):
-                ingredients.append(m.group('name'))
-        
-        ingredients = simplifyIngredients(ingredients)
-        ingredients = [list(x)[0] for x in ingredients]
-        ingredient_objs = []
-        for i in ingredients:
-            ingredient_obj = Ingredient(name=i)
-            ingredient_objs.append(ingredient_obj)
-            #ingredient_obj.save()
-            print ingredient_obj
-                
-        if('title' in recipe_data.keys() and 'instructions' in recipe_data.keys() and 'picture_link' in recipe_data.keys()):
-            recipe = Recipe(name=recipe_data['title'], 
-                     instructions=recipe_data['instructions'], 
-                     source=recipe_data['picture_link'])
-            print recipe
-            #recipe.save()
-            for i in ingredient_objs:
-                 recipe.ingredients.add(i)
-            recipes.append(recipe)
+        if('ingredients' in recipe_data.keys()):
+            for ingredient in recipe_data['ingredients']:
+                m = re.search('(?P<name>[a-zA-Z 0-9]+) ADVERTISEMENT', ingredient)
+                if(m):
+                    ingredients.append(m.group('name'))
             
-        if(len(recipes)==10):
+            ingredients = simplifyIngredients(ingredients)
+            ingredients = [list(x)[0] for x in ingredients]
+            ingredient_objs = []
+            for i in ingredients:
+                if(not i == "" and not i.isdigit()):
+                    ingredient_obj = Ingredient(name=i)
+                    ingredient_objs.append(ingredient_obj)
+                    ingredient_obj.save()
+                    print ingredient_obj
+                    
+            if('title' in recipe_data.keys() and 'instructions' in recipe_data.keys() and 'picture_link' in recipe_data.keys()):
+                recipe = Recipe(name=recipe_data['title'], 
+                         instructions=recipe_data['instructions'], 
+                         source=recipe_data['picture_link'])
+                print recipe
+                recipe.save()
+                for i in ingredient_objs:
+                     recipe.ingredients.add(i)
+                recipes.append(recipe)
+            
+        if(len(recipes)==30):
             break
